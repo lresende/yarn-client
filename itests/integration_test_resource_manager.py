b72b835ec154561e6948ef13e4395f398da6d3a8
Luciano Resende <lresende@apache.org>
2017-05-03 08:37:10 -0700

    Add integration tests to yarn client apis
    Validate api functionality against a running Yarn cluster.

---
+++ itests/integration_test_resource_manager.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+from pprint import pprint
+from unittest import TestCase
+from yarn_api_client.resource_manager import ResourceManager
+from yarn_api_client.errors import IllegalArgumentError
+
+class ResourceManagerTestCase(TestCase):
+    def setUp(self):
+        self.resourceManager = ResourceManager(serviceEndpoint='https://lresende-iop-cluster:8443/gateway/default/resourcemanager/v1/cluster',
+                                  username='guest',
+                                  password='guest-password')
+
+
+    def test_cluster_information(self):
+        info = self.resourceManager.cluster_information()
+        pprint(info.data)
+        self.assertEqual(info.data['clusterInfo']['state'], 'STARTED')
+
+
+
+    def test_cluster_metrics(self):
+        metrics = self.resourceManager.cluster_metrics()
+        pprint(metrics.data)
+        self.assertGreater(metrics.data['clusterMetrics']['activeNodes'], 0)
+        self.assertIsNotNone(metrics.data['clusterMetrics']['totalNodes'])
+
+
+    def test_cluster_scheduler(self):
+        scheduler = self.resourceManager.cluster_scheduler()
+        pprint(scheduler.data)
+        self.assertIsNotNone(scheduler.data['scheduler']['schedulerInfo'])
+
+
+    def test_cluster_applications(self):
+        apps = self.resourceManager.cluster_applications()
+        pprint(apps.data)
+        self.assertIsNotNone(apps.data['apps'])
+
+    def test_cluster_application_statistics(self):
+        appstats = self.resourceManager.cluster_application_statistics()
+        pprint(appstats.data)
+        self.assertIsNotNone(appstats.data['appStatInfo'])
+        # TODO: test arguments
+
+    def test_cluster_nodes(self):
+        nodes = self.resourceManager.cluster_nodes()
+        pprint(nodes.data)
+        self.assertIsNotNone(nodes.data['nodes'])
+
+        running_nodes = self.resourceManager.cluster_nodes(state='RUNNING', healthy='true')
+        pprint(running_nodes.data)
+        self.assertIsNotNone(nodes.data['nodes'])
+
+        running_nodes = self.resourceManager.cluster_nodes(state='NEW')
+        pprint(running_nodes.data)
+        self.assertIsNone(nodes.data['nodes'])
